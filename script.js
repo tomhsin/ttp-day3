87823602538b13e7f506ccf2bf04cb1de89bb30b
TomHsin <tom.hsin@thinkpower-info.com>
2020-02-15 15:18:37 +0800

    card-vue-v1

---
+++ script.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    name: 'Tom Hsin',
+    company: 'ThinkPower',
+    end: '2020/02/15',
+    cardClass: 'card',
+    imgSrc: "img/2cd43b_30926e419b424948a346f4a74f1f1e32_mv2.png",
+    bodyClass:"white"
+    }
+});
