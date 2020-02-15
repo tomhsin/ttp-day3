2ad8698f064e85abc50d918c2ad0e38b54478ca6
TomHsin <tom.hsin@thinkpower-info.com>
2020-02-15 18:03:32 +0800

    day3-end

--- script.js
+++ script.js
@@ -6,6 +6,32 @@ const app = new Vue({
     end: '2020/02/15',
     cardClass: 'card',
     imgSrc: "img/2cd43b_30926e419b424948a346f4a74f1f1e32_mv2.png",
-    bodyClass:"white"
-    }
+    newCompany: "",
+    newBg:"",
+    cleanBg:"",
+
+    showLife: true,
+    showSuper: true,
+
+    items:[
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '整个牛项目' },
+    ]
+    
+    },
+
+  methods:{
+      onSubmit(){
+          this.company = this.newCompany
+          this.newCompany =''
+      },
+
+      onChange(){
+          this.cardClass = this.newBg
+      },
+      onClean(){
+          this.newBg = this.cleanBg
+      }
+  }
 });
